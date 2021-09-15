42b3cd224d14636019036ae78e92c4124d359a3a
Kevin Newton <kddnewton@gmail.com>
2021-09-15 12:40:19 -0400

    Support the --ignore option

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'gemfilelint'
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "gemfilelint"
 
-require 'minitest/autorun'
+require "minitest/autorun"
